934a384b8b6615511bf96b9e072052f2c738b363
ahmed666916 <ahmed666916@gmail.com>
2024-02-08 21:33:32 +0500

    made handleButtonClick function work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
 const display = document.getElementById("calculator__output");
+const calculatorKeys = document.querySelector('.calculator__keys');
+
+
+calculatorKeys.addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        const buttonText = event.target.textContent;
+        console.log(buttonText);
+        handleButtonClick(buttonText);
+    }
+});
+
+
 
 function handleButtonClick(buttonText) {
     
