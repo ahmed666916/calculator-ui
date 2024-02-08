a0aa05a170833d834f3afc7a5f95b650b440704d
ahmed666916 <ahmed666916@gmail.com>
2024-02-08 21:39:58 +0500

    added condition for operator buttons

--- js/script.js
+++ js/script.js
@@ -6,8 +6,13 @@ const calculatorKeys = document.querySelector('.calculator__keys');
 calculatorKeys.addEventListener('click', function(event) {
     if (event.target.tagName === 'BUTTON') {
         const buttonText = event.target.textContent;
-        console.log(buttonText);
-        handleButtonClick(buttonText);
+        if (event.target.classList.contains('calculator__key--operator')) {
+            handleButtonClick(" " + buttonText + " ");
+        } else {
+            handleButtonClick(buttonText);
+        }
+     
+       
     }
 });
 
