d0ff98e84d5adaf6f58df9471c34a3392cc525c3
ahmed666916 <ahmed666916@gmail.com>
2024-02-09 18:24:15 +0500

    added add, subtract, multiply and divide functions

--- js/script.js
+++ js/script.js
@@ -46,3 +46,20 @@ function getInputText() {
     return inputArray;
 
 }
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
