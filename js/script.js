cfeee56a3ed66abdde9af0f13eab57cc7a8132e0
ahmed666916 <ahmed666916@gmail.com>
2024-02-09 18:23:12 +0500

    added getInputText function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const display = document.getElementById("calculator__output");
 const calculatorKeys = document.querySelector('.calculator__keys');
 
@@ -11,15 +10,39 @@ calculatorKeys.addEventListener('click', function(event) {
         } else {
             handleButtonClick(buttonText);
         }
-     
-       
+
+
     }
 });
 
 
 
 function handleButtonClick(buttonText) {
+
+
+	
+    display.innerHTML += `${buttonText}`;
+
+}
+
+function getInputText() {
+    let inputValue = display.innerHTML;
+    //console.log(inputValue);
+    //console.log(typeof(inputValue));
+    let numArray = inputValue.split(" ");
+    let num1 = parseFloat(numArray[0]);
+    let num2 = parseFloat(numArray[2]);
+    let operator = numArray[1];
+
+    //console.log(num1);
+    //console.log(num2);
+    //console.log(operator);
+    let inputArray = [];
+    inputArray.push(num1);
+    inputArray.push(num2);
+    inputArray.push(operator);
     
-   display.innerHTML += `${buttonText}`;
-    
+
+    return inputArray;
+
 }
